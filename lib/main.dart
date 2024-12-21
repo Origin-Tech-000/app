bd948c410d834b384d63eade1d6e85c161a6753c
Arun puca <codekeeper.github@gmail.com>
2024-12-21 17:03:50 +0530

    1

---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+// // import 'dart:html';
+// // import 'package:flutter/material.dart';
+
+// // void main() {
+// //   // Function to add the Propeller Ads script
+// //   final ScriptElement script = ScriptElement()
+// //     ..src = 'https://groleegni.net/401/8679963'
+// //     ..type = 'text/javascript';
+// //   try {
+// //     document.body?.append(script);
+// //   } catch (e) {
+// //     document.documentElement?.append(script);
+// //     print('Error appending script: $e');
+// //   }
+  
+// //   runApp(MyApp());
+// // }
+
+// // class MyApp extends StatelessWidget {
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return MaterialApp(
+// //       title: 'Propeller Ads Integration',
+// //       home: Scaffold(
+// //         appBar: AppBar(
+// //           title: Text('Propeller Ads Example'),
+// //         ),
+// //         body: Center(
+// //           child: Text('Propeller Ads script has been added.'),
+// //         ),
+// //       ),
+// //     );
+// //   }
+// // }
+// import 'dart:html';
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   // Function to add Propeller Ads scripts
+//   void addPropellerAdScript(String domain, int id) {
+//     final ScriptElement script = ScriptElement()
+//       ..src = 'https://$domain/401/$id'
+//       ..type = 'text/javascript';
+//     try {
+//       document.body?.append(script);
+//     } catch (e) {
+//       document.documentElement?.append(script);
+//       print('Error appending script: $e');
+//     }
+//   }
+
+//   // Add both scripts
+//   addPropellerAdScript('groleegni.net', 8679963);
+//   addPropellerAdScript('groleegni.net', 8680016);
+
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Propeller Ads Integration',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Propeller Ads Example'),
+//         ),
+//         body: Center(
+//           child: Text('Propeller Ads scripts have been added.'),
+//         ),
+//       ),
+//     );
+//   }
+// }
+// import 'dart:html';
+import 'package:flutter/material.dart';
+
+void main() {
+  // Function to add the Propeller Ads script
+  // final ScriptElement script = ScriptElement()
+  //   ..src = 'https://luglawhaulsano.net/4/8680039'
+  //   ..type = 'text/javascript';
+  // try {
+  //   document.body?.append(script);
+  // } catch (e) {
+  //   document.documentElement?.append(script);
+  //   print('Error appending script: $e');
+  // }
+  
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Propeller Ads Integration',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('e'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              
+            },
+            child: Text('    Test Appwrite'),)
+        ),
+      ),
+    );
+  }
+}
